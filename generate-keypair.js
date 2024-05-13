7c388b6087885eb641489afa3896f8878b2be750
thrishank <thrishankkalluru@gmail.com>
2024-05-13 11:47:25 +0530

    test

--- generate-keypair.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var web3_js_1 = require("@solana/web3.js");
-var key = web3_js_1.Keypair.generate();
-console.log("The public key is:" + key.publicKey.toBase58());
-console.log("The secret key is:" + key.secretKey);
