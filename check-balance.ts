c4fbf365638df45fd82aad14981bb0e13b776a7e
thrishank <thrishankkalluru@gmail.com>
2024-05-16 16:50:06 +0530

    transfer still not working

--- check-balance.ts
+++ check-balance.ts
@@ -1,9 +1,16 @@
 import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 import publicKey from "./generate-keypair";
 
-const connection = new Connection("https://api.devnet.solana.com", "confirmed");
-const address = new PublicKey(publicKey);
+const pubkey = process.argv[2] || publicKey;
+if (!pubkey) {
+  throw new Error("Provide a public key to check the balance of!");
+}
+
+const connection = new Connection("https://api.devnet.solana.com", "confirmed"); // Connecting to a solana cluster
+const address = new PublicKey(pubkey);
 
 const balance_lamports = await connection.getBalance(address);
 const balance_sol = balance_lamports / LAMPORTS_PER_SOL;
-console.log(balance_lamports, balance_sol);
+console.log(balance_lamports);
+
+export default connection;
