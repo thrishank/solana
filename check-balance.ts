c4a6f788debaa37a9066c5cbea817bcbd28684ac
thrishank <thrishankkalluru@gmail.com>
2024-06-27 11:17:06 +0530

    transaction done

--- check-balance.ts
+++ check-balance.ts
@@ -1,12 +1,12 @@
 import { getKeypairFromEnvironment } from "@solana-developers/helpers";
-import { Connection, LAMPORTS_PER_SOL, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 import "dotenv/config"
 
 const keyPair = getKeypairFromEnvironment("SECRET_KEY");
 
-const connect = new Connection(clusterApiUrl("devnet"))
+const connect = new Connection('https://api.devnet.solana.com', 'confirmed');
 const address = new PublicKey(keyPair.publicKey);
-// const address = new PublicKey("MJKqp326RZCHnAAbew9MDdui3iCKWco7fsK9sVuZTX2")
+// const address = new PublicKey("BD6BUUzunY1ToNmZeHZ9vBkgFqaSY336bzcCKaA2sUFM")
 
 const balance = await connect.getBalance(address) / LAMPORTS_PER_SOL;
 console.log(balance);
---
+++ transfer.ts
@@ -0,0 +1,24 @@
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+import { Connection, PublicKey, SystemProgram, Transaction, sendAndConfirmTransaction } from "@solana/web3.js";
+import "dotenv/config";
+
+const sendKeyPair = getKeypairFromEnvironment("SECRET_KEY");
+const toPubkey = new PublicKey("BD6BUUzunY1ToNmZeHZ9vBkgFqaSY336bzcCKaA2sUFM");
+
+const connect = new Connection("https://api.devnet.solana.com", "confirmed");
+
+const transaction = new Transaction();
+
+const LAMPORTS_TO_SEND = 100000000;
+
+const sendSolInstruction = SystemProgram.transfer({
+    fromPubkey: sendKeyPair.publicKey,
+    toPubkey,
+    lamports: LAMPORTS_TO_SEND
+})
+
+transaction.add(sendSolInstruction);
+
+const signature = await sendAndConfirmTransaction(connect, transaction, [sendKeyPair])
+
+console.log(signature);
